6cc9b3149c39a4e2ab677a8a3d800a015f80cf7b
Seth Shelnutt <Shelnutt2@gmail.com>
2014-04-02 08:39:39 -0400

    Add test.cpp

---
+++ Project/src/test.cpp
@@ -0,0 +1,16 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Apr 2, 2014
+ *      Author: shelnutt
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "This is my project" << endl;
+}
+
+
+
+
