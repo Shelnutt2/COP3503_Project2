7b4da0dae8e4e53bcadd9a4dbaeba5c3a7a090a4
Seth Shelnutt <Shelnutt2@gmail.com>
2014-04-03 18:03:55 -0400

    Remove Professor Name

--- Project/src/test.cpp
+++ Project/src/test.cpp
@@ -8,7 +8,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Professor: Joshua Horton" <<endl;
 	cout << "Lab: Thursday 5:10pm" <<endl;
 	cout << "Class: COP3503" << endl;
 	cout << "This is our project!" << endl;
